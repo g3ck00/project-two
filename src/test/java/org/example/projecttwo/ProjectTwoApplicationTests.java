64dcae68ba015e4fa1bd88d798f9d62635c21a90
g3ck00 <g3ck0@protonmail.ch>
2026-06-15 02:47:35 -0500

    init

---
+++ src/test/java/org/example/projecttwo/ProjectTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.projecttwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectTwoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
